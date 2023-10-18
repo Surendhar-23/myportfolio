b78177d66e1df72d304c15d6da591c32769ee1de
Surendhar-23 <ndsurendhar2004@gmail.com>
2023-10-18 08:40:21 +0530

    latest

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-document.q;
+let displaypercent = function (element, value) {
+  let counter = 0;
+  return function () {
+    if (counter == value) {
+      clearInterval(intervalId);
+    } else {
+      counter += 1;
+      element.innerHTML = counter + "%";
+    }
+  };
+};
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  delay: 200,
+  reset: true,
+});
+
+sr.reveal(
+  ".hero-section,.about-section,.skill-section,.education-section,.project-section,.contact-section,.footer",
+  {}
+);
+sr.reveal("nav-link", {
+  delay: 400,
+});
+sr.reveal(".home__social-icon", { interval: 200 });
+sr.reveal(".main-content .main-img", { interval: 200 });
